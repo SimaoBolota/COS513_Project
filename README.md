3317a2224938150c575715cf0bd2a710eba4d496
Simão sá <simaosa@Simaos-MacBook-Pro.local>
2022-10-22 17:24:55 +0100

    commenting

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Project created for COS513 - Computational Intelligent Systems
 ## Dataset
 
 
-The dataset retrieved from kaggle (https://www.kaggle.com/datasets/navoneel/brain-mri-images-for-brain-tumor-detection) reffers to MRI images of a healthy and non-healthy brain ```brain_tunor_dataset```. The non-healthy brain can be seen as a brain that contains a tumor.
+The dataset retrieved from kaggle (https://www.kaggle.com/datasets/navoneel/brain-mri-images-for-brain-tumor-detection) reffers to MRI images of a healthy and non-healthy brain ```brain_tumor_dataset```. The non-healthy brain can be seen as a brain that contains a tumor.
 This dataset is divided into 
 
 - yes folder, containing all the brain images with a tumor identified, 155 images
@@ -16,13 +16,41 @@ Making a total of 253 images.
 
 ## Pre-Processing
 
-For pre-processing steps the image data is standardized and transformed from rgb (...,3) to greyscale (...,1). 
+Data pre-processing is the process in which raw data is transformed into an understandable and desired format, assessing and improving the data quality. This can be done in several ways, such as:
+- Scaling
+- Re-shaping
+- Colour re-scaling
 
-After this pre-processing tha image data is the following (16 random examples):
-![Figure_1](https://user-images.githubusercontent.com/55351724/196028202-ec6aa65f-a222-4e9f-bd54-cd7ec1e13194.png)
+,which were the pre-processing steps taken in this project.
 
-**False** meaning the brain is not healthy and **True** meaning the brain is healthy
+## Splitting data
+
+For the current dataset, 25% of the complete dataset is used as the Testing Dataset while the remaining 75% of the complete dataset is used as the Training Dataset:
+- Training image data shape - (202, 224, 224, 1)
+- Training label data shape - (202, 1)
+- Testing image data shape - (51, 224, 224, 1)
+- Testing label data shape - (51, 1)
 
 ## Modelling
 
-to do 
+- In this project three convolution layers are used, each applying padding to the input image so that the input image gets fully covered by the filter and specified stride ( 1 by 1 moving one pixel at a time)
+- After each convolution layer a dropout layer is added. The fraction of input units to drop used in the dropout layer for this project is 0.15
+- After each convolution layer a pooling layer is added. The pooling layer used takes in consideration its default values, having a pooling kernel of (2,2)
+
+
+## Performance Evaluation
+
+To assess the model performance, different measures can be used. Mos of these performance measures/metrics are based on the comparison between the model prediction’s and the expected (known) values. The performance metrics used to evaluate the model created in this project are:
+- Loss
+- Accuracy
+- F1 score
+
+## Results
+
+The created model is successful in classifying a random brain MRI image as healthy or tumorous returning good performance and evaluation metrics, as you can see below in the model statistics table
+
+
+On the other hand, a task as complex as identifying a healthy or non-healthy brain has no space for errors. In the real world if a patient would to be identified as having a tumour while on reality it didn’t, could be catastrophic for someone’s life as the other way around would as well. The created model would create some of these concerns as it can misclassify some of the given input images (‘P’ meaning the predicted brain status and ‘R’ meaning the real brain status)
+
+
+For this reason, there is always space for improvements until the loss value is closer to 0, the accuracy is practically 100% and the F1 score is approximately 1, meaning all cases get covered and the model predicts the outputs correctly for all possible inputs.
--- something.py
+++ something.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
@@ -9,54 +8,56 @@ from keras.optimizers import RMSprop,SGD,Adam
 from keras import datasets, layers, models
 from keras import metrics
 import statistics
-
 import tensorflow as tf
-
 import cv2
 import os
-import pdb
-
 import numpy as np
 import random
 from sklearn.model_selection import train_test_split
 import pdb
-
 from rich.console import Console
 from rich.table import Table
-# 155 yes + 98 no's = 253 pics total
-#   1(no tumor) or 0 (tumor) 
+from keras import backend as K
+
+
+## ACQUIRING DATA ##
 
 labels = ['yes', 'no']
 img_size = 224
+
+#function to acquire data and its labels
 def get_data(data_dir):
     data = [] 
     for label in labels: 
         path = os.path.join(data_dir, label)
         class_num = labels.index(label)
-        print(path)
         
         for img in os.listdir(path):
             try:
-                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
-                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                #convert BGR to RGB format
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] 
+                # Reshaping images to preferred size
+                resized_arr = cv2.resize(img_arr, (img_size, img_size))
                 data.append([resized_arr, class_num])
+                # randomize and shuffle data
                 random.shuffle(data)
             except Exception as e:
                 print(e)
     
     return np.asanyarray(data)
 
-######################## GETTING IMAGE DATA
 
-
+#get the image data from the brain_tumor_dataset
 all_data = get_data('COS513_Project/brain_tumor_dataset/')
 
 x_data = []
 y_data = []
  # Iterate over the Data
 for x in all_data:
-    x_data.append(x[0])        # Get the X_Data
-    y_data.append(x[1])        # get the label
+    # Get the image data
+    x_data.append(x[0])  
+    # get the label      
+    y_data.append(x[1])        
 
     X_Data = np.asarray(x_data)      
     Y_Data = np.asarray(y_data)
@@ -66,8 +67,9 @@ for x in all_data:
     Y_Data = Y_Data.reshape(-1, 1)
 
 
-######################## PRE-PROCESSING 
+## PRE-PROCESSING PHASE ##
 
+#standardize function
 def standardize(image_data):
         image_data = image_data.astype(float)
         mean = np.mean(image_data, axis=0)
@@ -76,15 +78,16 @@ def standardize(image_data):
         image_data /= std
         return image_data, mean, std
 
+#standardize the image data
 X_Data, mean, std =   standardize(X_Data)
 
-
+#perform colour scaling to the image data (grey scale)
 X_Data = tf.image.rgb_to_grayscale(X_Data)
 
+# print(X_Data.shape)
+# print(Y_Data.shape)
 
-print(X_Data.shape)
-print(Y_Data.shape)
-
+#counting the data per classification label
 healthy_count = 0
 tumour_count = 0
 for label in Y_Data:
@@ -93,12 +96,8 @@ for label in Y_Data:
     else:
         healthy_count = healthy_count + 1
 
-print('\nHealthy pics count:')        
-print(healthy_count)
-print('\nTumour pics count')
-print(tumour_count)
 
-
+#create bar plot of the data classified by label
 plot_labels = ['Healthy', 'Tumour']
 plot_count = [healthy_count, tumour_count]
   
@@ -106,35 +105,13 @@ fig = plt.figure(figsize = (10, 5))
  
 plt.bar(plot_labels, plot_count, color ='blue',
         width = 0.4)
- 
 plt.xlabel("Brain Status")
 plt.ylabel("No. of brain MRI images")
 plt.title("Brain MRI images count comparison")
 plt.show()
-    
-######################## DIVIDE DATA
-
-# IS THE BRAIN HEALTHY
-#FALSE - tumor - 0
-#TRUE - no tumor - 1
-
-num_test_images = round(X_Data.shape[0] * 0.2)
-num_train_images =  X_Data.shape[0] - num_test_images
-
-x_train = X_Data[:num_train_images] #find better way do divide data
-y_train = Y_Data[:num_train_images] #find better way do divide data
-x_test = X_Data[num_train_images:] #find better way do divide data
-y_test = Y_Data[num_train_images:] #find better way do divide data
-
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
 
 
-
-######################## PLOTTING IMAGES
-
+#plot the image data and its labels
 def plot_multi(i):
     '''Plots 16 digits, starting with digit i'''
     nplots = 16
@@ -142,15 +119,34 @@ def plot_multi(i):
     for j in range(nplots):
         plt.subplot(4,4,j+1)
         plt.imshow(X_Data[i+j], cmap='binary')
-        # plt.title(Y_Data[i+j])
+        plt.title(Y_Data[i+j])
         plt.axis('off')
     plt.show()
 
 plot_multi(190)
 
-######################## BUILDING MODEL
-from keras import backend as K
 
+## DIVIDE DATA PHASE ##
+
+#getting the number of images to be included in the testing set (25% of the data)
+num_test_images = round(X_Data.shape[0] * 0.25)
+#getting the number of images to be included in the training set
+num_train_images =  X_Data.shape[0] - num_test_images
+#dividing daia into training and testing sets
+x_train = X_Data[:num_train_images] 
+y_train = Y_Data[:num_train_images] 
+x_test = X_Data[num_train_images:] 
+y_test = Y_Data[num_train_images:] 
+
+# print(x_train.shape)
+# print(y_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
+
+
+## MODEL BUILDING PHASE ##
+
+#recall calculation function
 def recall(y_true, y_pred):
     y_true = K.ones_like(y_true) 
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -159,6 +155,7 @@ def recall(y_true, y_pred):
     recall_m = true_positives / (all_positives + K.epsilon())
     return recall_m
 
+#precision calculation function
 def precision(y_true, y_pred):
     y_true = K.ones_like(y_true) 
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
@@ -167,11 +164,13 @@ def precision(y_true, y_pred):
     precision_m = true_positives / (predicted_positives + K.epsilon())
     return precision_m
 
+#f1 score calculation function
 def f1_score(y_true, y_pred):
     precision_m = precision(y_true, y_pred)
     recall_m = recall(y_true, y_pred)
     return 2*((precision_m*recall_m)/(precision_m+recall_m+K.epsilon()))
 
+#creating the sequential model layers
 model = models.Sequential()
 model.add(layers.Conv2D(64, (3, 3), padding = 'SAME', activation='relu', input_shape=(224, 224, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -182,26 +181,32 @@ model.add(tf.keras.layers.Dropout(0.15))
 model.add(layers.Conv2D(64, (3, 3), padding = 'SAME', activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 
+#flatten the layers
 model.add(layers.Flatten())
-
 model.add(layers.Dense(10, activation='softmax'))
 
+#compile and set the performance metrics of the model
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy', metrics.categorical_accuracy, metrics.mean_absolute_error,f1_score ])
 
 
 model.summary()
-######################## TRAINING
 
+## TRAINING PHASE ##
+
+#getting the validation data
 validation_data_ = (x_test, y_test)
-                
+
+#training model with the train test set                
 history = model.fit(x_train, y_train, epochs=25, batch_size=40, 
                     validation_data=validation_data_)
 
 
 
-######################## VALIDATION AND PLOTS
+## VALIDATION PHASE ##
+
+#plotting the accuracy per epoch
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
@@ -209,33 +214,18 @@ plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
 plt.show()
 
-
-plt.plot(history.history['mean_absolute_error'], label='mean_absolute_error')
-plt.xlabel('Epoch')
-plt.ylabel('mean_absolute_error')
-plt.legend(loc='upper right')
-plt.show()
-
+#plotting the loss per epoch
 plt.plot(history.history['loss'], label='loss')
 plt.xlabel('Epoch')
 plt.ylabel('loss')
 plt.legend(loc='upper right')
 plt.show()
 
-
-plt.plot(history.history['f1_score'], label='f1 score')
-plt.xlabel('Epoch')
-plt.ylabel('f1 score')
-plt.legend(loc='lower right')
-ax = plt.gca()
-ax.set_ylim([0, 1])
-plt.show()
-
-
+# getting the evaluation metrics for the used model
 (loss, accuracy, f1_score, precision, recall) = model.evaluate(x_test, y_test, verbose=1)
 
 
-
+#creating styles for the table, red for a bad performance metric value and green for a good performance metric value
 if(loss)>1.0:
     loss_style = 'red'
 else:
@@ -256,29 +246,21 @@ if(f1_score)<0.6:
 else:
     f1_style = 'green'
 
-
-
+# creating the model Statistics table
 table = Table(title="Stats on the model")
 table.add_column("Loss", justify="right", style=loss_style)
 table.add_column("Accuracy", style=accuracy_style)
 table.add_column("val_accuracy", style=val_accuracy_style)
 table.add_column("F1 score", justify="right", style=f1_style, no_wrap=True)
-
 table.add_row(str(loss), str(statistics.mean(history.history['accuracy'])),str(accuracy), str(f1_score))
-# table.add_row('0.3825118', '0.8989217643', '0.843137245', '0.745654738')
-
 console = Console()
 console.print(table)
 
-
-predictions = model.predict(x_test)
-predictions_index = np.argmax(predictions, axis=1) # Convert one-hot to index; remember indexing starts from 0; index takes integers values in [0,9]
-predictions_index
-
-
-classname = ['Tumor','Healthy']
-
 # Plot a random sample of 15 test images, their predicted labels and ground truth
+classname = ['Tumor','Healthy']
+predictions = model.predict(x_test)
+predictions_index = np.argmax(predictions, axis=1) 
+predictions_index
 figure = plt.figure(figsize=(20, 20))
 for i, index in enumerate(np.random.choice(x_test.shape[0], size=15, replace=False)):
     ax = figure.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
